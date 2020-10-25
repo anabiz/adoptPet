dff68b169a306cbac4fce42c22ae003d4363a2cc
apple <apple@Anabiz.local>
2020-10-25 07:35:23 +0100

    view and select pet for adoption

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import useCustom from './components/customHook';
+import Searchpet1 from "./components/petwithcustomhook";
+import Searchpet from "./components/pet";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Searchpet1 />
+    </div>
+  );
+}
+
+export default App;
